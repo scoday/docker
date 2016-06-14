dafea899b3e550563141ba9dd9e3de85816c1040
Scott Day <scoday@users.noreply.github.com>
2016-06-14 12:55:37 +0900

    Initial commit of this interesting script
    This creates a btrfs mirror, then sets up gitlab.

---
+++ usb-gitlab-docker.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -x
+#
+# Use this guideline to make a gitlab on USB disks or 
+# realistically any type of disks. For this I used two
+# USB thumbs + btrfs mirror to create a docker gitlab.
+# Some data taken from here: https://blog.sixeyed.com/run-gitlab-on-a-usb-stick-with-docker/
+# The rest all from my cranial processing unit.
+# 
+
+create-btrfs() {
+	echo -n "Be forewarned, whatever you put here will be DESTROYED."
+	echo -n "Please type drive ids (e.g. /dev/sdd /dev/sdb): "
+	  read answer
+	  mkfs.btrfs -L DatenSpeicher -m raid1 -d raid1 $answer -f
+}
+
+create-gitlabmnt() {
+	echo -n "This will create a partition on / named gitlab."
+	mkdir /gitlab && mount -L DatenSpeicher /gitlab
+}
+
+create-gitdirs() {
+	echo -n "This will create the requisite directories."
+	mkdir /gitlab/{config,logs,data}
+}
+
+create-docker-yml() {
+echo -n "Will create the docker yml file now."
+echo "
+gitlab:  
+ container_name: gitlab
+ image: gitlab/gitlab-ce:8.5.3-ce.0
+ hostname: usb-gitlab
+ environment:
+   GITLAB_OMNIBUS_CONFIG: |
+     external_url 'http://127.0.0.1:8050'
+     gitlab_rails['gitlab_shell_ssh_port'] = 522
+     ports:
+      - '8050:8050'
+      - '522:22'
+     volumes:
+      - /gitlab/config:/etc/gitlab
+      - /gitlab/logs:/var/log/gitlab
+      - /gitlab/data:/var/opt/gitlab
+ privileged: true
+" > /gitlab/docker-compose.yml
+}
+
+show-docker-ids() {
+	docker ps
+}
+
+start-docker() {
+	cd /gitlab
+	docker-compose up -d
+}
+
+create-btrfs
+create-gitlabmnt
+create-gitdirs
+create-docker-yml
+start-docker
+show-docker-ids
